69928f7488d254b39f31c323520e5b9d27581c25
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-14 01:37:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.map do |season, holiday|
-  holiday.map do |holiday, supply|
+  holiday_hash.map do |holiday, supply|
     hoiliday if supply.unclude?(BBQ)
   end
 end.flatten.compact
